84966f882ebc1d40dff94b3c93cff29c85cb9350
Sydowh <Sydowh31@gmail.com>
2016-05-16 20:37:57 +0200

    Maps

--- wp-content/themes/owlgraphics/template-accueil.php
+++ wp-content/themes/owlgraphics/template-accueil.php
@@ -87,6 +87,26 @@ get_header(); ?>
             </section>
             <section id="contact">
                 <div class="container">
+                    <?php echo do_shortcode("[contact-form-7 id=\"17\" title=\"Formulaire de contact\"]"); ?>
+                    <div id="map"></div>
+                    <script type="text/javascript">
+                        google.maps.event.addDomListener(window, 'load', init);
+                        var myLatlng = new google.maps.LatLng(48.84817719999999, 2.3411866000000146);
+                        function init() {
+                            var mapOptions = {
+                                zoom: 15,
+                                center: myLatlng,
+                                styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"},{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"simplified"},{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]}]
+                            };
+                            var mapElement = document.getElementById('map');
+                            var map = new google.maps.Map(mapElement, mapOptions);
+                            var marker = new google.maps.Marker({
+                                position: myLatlng,
+                                map: map
+                            });
+                        }
+                    </script>
+                    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=&#9;&#10;AIzaSyBC-8UDSRxFvrfrK_lPv3Gx2wLMCwLdw5Q&amp;sensor=false"></script>
                 </div>
             </section>
         </div><!-- .entry-content-page -->
